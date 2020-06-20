049eda849278935f43d467af90dfd2963ff4b2da
Sergey <ryabovs@yahoo.com>
2020-06-20 16:27:23 +0200

    introduces start and tick methods

--- index.ts
+++ index.ts
@@ -1,16 +1,35 @@
 interface Field {
-    [height: number]: {
-        [width: number]: boolean
+    [row: number]: {
+        [element: number]: boolean
     }
 }
 
 class GameOfLife {
     public field: Field
+    private gameSpeed = 100000
 
     constructor(size: number)
     constructor(height: number, width?: number)
     constructor(height: number, width = height) {
         this.field = this.generateField(height, width)
+    }
+
+    public start(): void {
+        setInterval(this.tick, this.gameSpeed)
+    }
+
+    private tick = () => {
+        for (const row of Object.keys(this.field)) {
+            for (const element of Object.keys(this.field[+row])) {
+                const neighbours = this.getNeighbours(+row, +element)
+            }
+        }
+    }
+
+    private getNeighbours(row: number, element: number): Array<boolean> {
+        const neighbours = [false]
+
+        return neighbours
     }
 
     private generateField(size: number): Field
@@ -35,3 +54,4 @@ class GameOfLife {
 }
 
 const game = new GameOfLife(5)
+game.start()
